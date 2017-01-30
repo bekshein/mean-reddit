aca8d84d967df3b0e3f0e2ef3466b1ca8b2cbe9e
Rebekah Shein <rmshein@gmail.com>
2017-01-30 01:52:34 -0500

    Create config block with home state

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,5 +1,16 @@
 var app = angular.module('meanReddit', ['ui.router']);
 
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl',
+    });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+
 app.factory('posts', [function () {
   var o = {
     posts: []
