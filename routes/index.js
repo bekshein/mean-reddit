7d0466fc598cfde637f9403c0f85037b94c0e710
Rebekah Shein <rmshein@gmail.com>
2017-01-30 03:34:19 -0500

    Create register user route

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// register authentication and return JWT token
+router.post('/register', function (req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  var newUser = new User();
+  newUser.username = req.body.username;
+  newUser.setPassword(req.body.password);
+
+  newUser.save(function (err) {
+    if (err) { return next(err); }
+
+    return res.json({ token: newUser.generateJWT() });
+  });
+});
+
 
 // index all posts
 router.get('/posts', function (req, res, next) {
