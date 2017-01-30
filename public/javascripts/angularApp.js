8e984df3b901963b5411284f8d4fe1753f274635
Rebekah Shein <rmshein@gmail.com>
2017-01-30 04:09:15 -0500

    Create login and register states both with onEnter properties

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -21,6 +21,26 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
           return posts.get($stateParams.id);
         }]
       }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function ($state, auth) {
+        if (auth.isLoggedIn()) {
+          $state.go('home');
+        }
+      }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function ($state, auth) {
+        if (auth.isLoggedIn()) {
+          $state.go('home');
+        }
+      }]
     });
 
     $urlRouterProvider.otherwise('home');
