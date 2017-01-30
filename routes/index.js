1d584f4ead9f5e11219562fc161f9a7fa4206566
Rebekah Shein <rmshein@gmail.com>
2017-01-30 03:36:01 -0500

    Create login user route

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,22 @@ router.post('/register', function (req, res, next) {
   });
 });
 
+// login authentication and return JWT token
+router.post('/login', function (req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  passport.authenticate('local', function (err, user, info) {
+    if (err) { return next(err); }
+
+    if (user) {
+      return res.json({ token: user.generateJWT() });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
 
 // index all posts
 router.get('/posts', function (req, res, next) {
