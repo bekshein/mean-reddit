d980d6b075adeddddae1866b2cdaab511818a722
Rebekah Shein <rmshein@gmail.com>
2017-01-30 02:28:12 -0500

    Create middleware function to preload post object for post ID route handlers

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,18 @@ router.post('/posts', function (req, res, next) {
     res.json(post);
   });
 });
+
+// middleware function to preload post object to handle post ID routes (will retrieve and attach post object to the req object for route handlers with :post route parameter)
+router.param('post', function (req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
 module.exports = router;
