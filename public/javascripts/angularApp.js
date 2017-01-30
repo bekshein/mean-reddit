f6242636fc2854df68ce9879146ba811a4255fe4
Rebekah Shein <rmshein@gmail.com>
2017-01-30 04:47:53 -0500

    Remove hard coded author property in addComment method

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -213,7 +213,6 @@ app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth', function ($scope
     if (!$scope.body || $scope.body === '') { return; }
     posts.addComment(post._id, {
       body: $scope.body,
-      author: 'user'
     }).success(function (comment) {
       $scope.post.comments.push(comment);
     });
